aa170a5ff28b73b2b902bea83590eaba5ef88912
Alvacampos <gonzaloralvarezcampos@gmail.com>
2024-07-20 20:00:07 -0300

    ssr to false

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
   plugins: [
     remix({
       basename: '/portfolio-ts-react/',
+      ssr: false,
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
